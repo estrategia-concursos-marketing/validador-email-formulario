5513cdf19ff7d11e257e439563c9a47927442192
Israel Mendes <38436684+israelmendez232@users.noreply.github.com>
2019-09-16 14:50:16 -0300

    Modificações para funcionar o GET.

--- handler.py
+++ handler.py
@@ -5,26 +5,21 @@ import dns.resolver
 import urllib.parse
 
 """
- MICROSERVIÇO para levar as leads do formulário aos respectivos lugares. 
-  Criado com framework serverless. Usado a conta do MKT/BI para hospedar o microserviço. Para acessar o repositório, confira:
-    https://github.com/estrategia-concursos-marketing/microservico-marketing-salescloud
 
-  PROCESSO:
-  1. Valida o email, respondendo sucesso ou não. Resposta no front se não for um email válido;
-  2. Envia os dados para o Marketing Cloud nas respectivas bases:
-    a. Leads_Gerais_5
-    b. Total_Base_Geral_
-    Lembrando que para acessar essas informações, elas estão salvas como variáveis no Lambda.
-  3. Envia os dados para as leads no SalesCloud, para uso do comercial.
+ MICROSERVIÇO para validar as leads do email para o formulário. 
+ Lembrando que o REGEX é validado no front, mas aqui é feito pelo MX e SMTP.
+ Qualquer PROBLEMA, entre em contato com a equipe de BI e CRM, responsáveis pela produção desse microserviço.
 
-  Qualquer PROBLEMA, entre em contato com a equipe de BI e CRM, responsáveis pela produção desse microserviço.
 """
 
 # context = '1'
 # event = {"body": "data=%7B%22oid%22%3A%2200D41000001Q9k8%22%2C%22retURL%22%3A%22https%3A%2F%2Fwww.estrategiaconcursos.com.br%2Fgratis%2Fsucesso%2F%22%2C%22Cidade_OrigemIP__c%22%3A%22Barueri%22%2C%22Estado_OrigemIP__c%22%3A%22Sao+Paulo%22%2C%22Modo_de_entrada__c%22%3A%22landing-page%22%2C%22lead_source%22%3A%22Landing+Page%22%2C%22Area_de_Interesse__c%22%3A%22tribunais%22%2C%22Concurso_de_Interesse__c%22%3A%22%22%2C%22Interesse_Evento__c%22%3A%22%22%2C%22recordType%22%3A%2201241000001AP21%22%2C%22first_name%22%3A%22israel%22%2C%22email%22%3A%22israel.mendes%40estrategiaconcursos.com.br%22%2C%22phone%22%3A%22(55)+11944-6919%22%7D", "isBase64Encoded": 0}
 
 def verify(event, context):
+    print("Event Antes da Transformação: ", event)
+    print("Context Antes da Transformação: ", context)
     event1 = json.loads(urllib.parse.parse_qs(event['body'])['data'][0])
+    print("Event Pós-Transformação: ", event1)
     
     def emailValidator(email):
         splitAddress = email.split('@')
@@ -54,6 +49,7 @@ def verify(event, context):
             return 'notExistingEmail'
     
     email = event1['email']
+    print("Email: ", email)
 
     # Principal do Microserviço
     if emailValidator(email) == 'notExistingEmail':
@@ -64,8 +60,8 @@ def verify(event, context):
         response = {
             'statusCode': 400,
             'headers': {
-                "Access-Control-Allow-Credentials": True,
                 "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": True,
                 "Content-Type": "application/json"
             },
             'body': json.dumps(body)
@@ -80,8 +76,8 @@ def verify(event, context):
         response = {
             'statusCode': 200,
             'headers': {
-                "Access-Control-Allow-Credentials": True,
                 "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": True,
                 "Content-Type": "application/json"
             },
             'body': json.dumps(body)
