72936f8e208e709b14ff96c1fa0590997e6d2b9c
Israel Mendes <38436684+israelmendez232@users.noreply.github.com>
2019-09-18 15:20:20 -0300

    Ajustes no código para funcionamento no AWS. Ainda com erros de 504

--- handler.py
+++ handler.py
@@ -13,13 +13,14 @@ import urllib.parse
 """
 
 # context = '1'
-# event = {"body": "data=%7B%22oid%22%3A%2200D41000001Q9k8%22%2C%22retURL%22%3A%22https%3A%2F%2Fwww.estrategiaconcursos.com.br%2Fgratis%2Fsucesso%2F%22%2C%22Cidade_OrigemIP__c%22%3A%22Barueri%22%2C%22Estado_OrigemIP__c%22%3A%22Sao+Paulo%22%2C%22Modo_de_entrada__c%22%3A%22landing-page%22%2C%22lead_source%22%3A%22Landing+Page%22%2C%22Area_de_Interesse__c%22%3A%22tribunais%22%2C%22Concurso_de_Interesse__c%22%3A%22%22%2C%22Interesse_Evento__c%22%3A%22%22%2C%22recordType%22%3A%2201241000001AP21%22%2C%22first_name%22%3A%22israel%22%2C%22email%22%3A%22israel.mendes%40estrategiaconcursos.com.br%22%2C%22phone%22%3A%22(55)+11944-6919%22%7D", "isBase64Encoded": 0}
+# with open('resposta.json') as f:
+#     event = json.load(f)
+#     print(event)
 
 def verify(event, context):
-    print("Event Antes da Transformação: ", event)
-    print("Context Antes da Transformação: ", context)
-    event1 = json.loads(urllib.parse.parse_qs(event['body'])['data'][0])
-    print("Event Pós-Transformação: ", event1)
+    email = event['queryStringParameters']['address']
+    # print(email)
+    # print("Event Antes da Transformação: ", event)
     
     def emailValidator(email):
         splitAddress = email.split('@')
@@ -47,40 +48,37 @@ def verify(event, context):
             return 'existingEmail'
         else:
             return 'notExistingEmail'
-    
-    email = event1['email']
-    print("Email: ", email)
 
     # Principal do Microserviço
     if emailValidator(email) == 'notExistingEmail':
         body = {
-            'message': 'Email nao existente. Retornar para o usuario.',
-            'input': email
+            "message": "Email nao existente. Retornar para o usuario.",
+            "input": email
         }
         response = {
-            'statusCode': 400,
-            'headers': {
+            "statusCode": 400,
+            "headers": {
                 "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Credentials": True,
                 "Content-Type": "application/json"
             },
-            'body': json.dumps(body)
+            "body": json.dumps(body)
         }
         print(response)
         return response
     else:
         body = {
-            'message': 'Email existente. Continue.',
-            'input': email
+            "message": "Email existente. Continue.",
+            "input": email
         }
         response = {
-            'statusCode': 200,
-            'headers': {
+            "statusCode": 200,
+            "headers": {
                 "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Credentials": True,
                 "Content-Type": "application/json"
             },
-            'body': json.dumps(body)
+            "body": json.dumps(body)
         }
         print(response)
         return response
